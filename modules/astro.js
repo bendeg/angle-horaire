54f716b90f54e823b69f7b653e7159bfffb76d88
bendeg <benoitdeg@gmail.com>
2022-06-08 13:29:52 +0200

    refactorisation ra + DMS/HMS

--- modules/astro.js
+++ modules/astro.js
@@ -1,79 +1,17 @@
 import * as AA from "./astronomical-algorithms.js";
 import * as Inputs from "./inputs.js";
 
-let 
-    rightAscension,
-    declination,
-
-    lat = new Inputs.InputTextDMS("lat", "divlat", "0.0"),
-    lon = new Inputs.InputTextDMS("lon", "divlon", "0.0"),
-
-    addeg = document.getElementById("addeg"),
-    adh = document.getElementById("adh"),
-    adm = document.getElementById("adm"),
-    ads = document.getElementById("ads"),
-
-    decdeg = document.getElementById("decdeg"),
-    decd = document.getElementById("decd"),
-    decm = document.getElementById("decm"),
-    decs = document.getElementById("decs"),
+let lat = new Inputs.InputTextDMSHMS("divlat", "0.0", true),
+    lon = new Inputs.InputTextDMSHMS("divlon", "0.0", true),
+    ra = new Inputs.InputTextDMSHMS("divra", "0.0", false),//false car HMS
+    dec = new Inputs.InputTextDMSHMS("divdec", "0.0", true),
 
     localsideraltime = document.getElementById("localsideraltime"),
     hourangle = document.getElementById("hourangle"),
     azimuth = document.getElementById("azimuth"),
     hauteur = document.getElementById("hauteur");
 
-addeg.oninput = changeRADegre;
-adh.oninput = changeRA;
-adm.oninput = changeRA;
-ads.oninput = changeRA;
-
-decdeg.oninput = changeDECDegre;
-decd.oninput = changeDEC;
-decm.oninput = changeDEC;
-decs.oninput = changeDEC;
-
-changeRA();
-changeDEC();
 mainLoop();
-
-function changeRADegre() {
-  var temp = addeg.value / 15;
-
-  rightAscension = addeg.value;
-
-  adh.value = Math.trunc(temp);
-  temp = (temp - adh.value) * 60;
-  adm.value = Math.trunc(temp);
-  ads.value = (temp - adm.value) * 60;
-}
-
-function changeRA() { 
-  rightAscension = Math.trunc(parseFloat(adh.value)) * 15
-      + Math.trunc(parseFloat(adm.value)) / 60 * 15
-      + parseFloat(ads.value) / 3600 * 15;
-
-  addeg.value = rightAscension;
-}
-
-function changeDECDegre() {
-  var temp = decdeg.value;
-
-  decd.value = Math.trunc(temp);
-  temp = (temp - decd.value) * 60;
-  decm.value = Math.trunc(temp);
-  decs.value = (temp - decm.value) * 60;
-}
-
-function changeDEC() { 
-  declination = (
-      Math.trunc(parseFloat(decd.value))
-      + Math.trunc(parseFloat(decm.value)) / 60
-      + parseFloat(decs.value) / 3600
-      );
-  
-  decdeg.value = declination;
-}
 
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -90,41 +28,16 @@ async function mainLoop() {
     lst = AA.Algorithms.localSideralTime(parseFloat(lon.getValue()) * (document.getElementsByName("greenwichmeridian")[1].checked ? 1 : -1) );
     localsideraltime.innerText = AA.Algorithms.degreeToHMS(lst);
     
-    ha = lst - rightAscension;
+    ha = lst - parseFloat(ra.getValue());
     hourangle.innerText = AA.Algorithms.degreeToHMS(ha);
 
     azimutalCoordinates = AA.Algorithms.equatorialToAzimutal(
                               ha, 
                               parseFloat(lat.getValue()) * (document.getElementsByName("hemisphere")[0].checked ? 1 : -1),
-                              declination * (document.getElementsByName("dechemisphere")[0].checked ? 1 : -1)
+                              parseFloat(dec.getValue()) * (document.getElementsByName("dechemisphere")[0].checked ? 1 : -1)
                               );
     azimuth.innerText = azimutalCoordinates.A;
     hauteur.innerText = azimutalCoordinates.h >= 0 ? azimutalCoordinates.h : "sous l'horizon (" + azimutalCoordinates.h + ")";
   }
 }
 
-// function create(id, parent, width, height) {
-//   let divrow = document.createElement('div');
-//   let divcolLabel = document.createElement('div');
-
-//   let canvasElem = document.createElement('canvas');
-//   parent.appendChild(divrow);
-//   divrow.appendChild(divcolLabel);
-//   divWrapper.appendChild(canvasElem);
-
-//   divrow.id = id;
-//   divrow.className = "row border rounded mb-2 p-3";
-//   divcolLabel.id = id + "-label";
-//   divcolLabel.className = "col-md-6";
-//   divcolLabel.innerHTML = "Test label";
-//   canvasElem.width = width;
-//   canvasElem.height = height;
-
-//   let ctx = canvasElem.getContext('2d');
-
-//   return {
-//    ctx: ctx,
-//    id: id
-//   };
-//   return divrow;
-// }
--- modules/inputs.js
+++ modules/inputs.js
@@ -1,6 +1,6 @@
 /*
 Permet l'ajout de 4 'input' type text
-1 'input' en degrés décimaux et 3 inputs pour les degrés DMS.
+1 'input' en degrés décimaux et 3 inputs pour les degrés DMS ou HMS (3ème argument du constructeur : true=DMS par défaut, false=HMS).
 Modifier l'input en degrés décimaux modifie les 3 inputs DMS.
 Modifier un des 3 input DMS modifie l'input des degrés décimaux.
 
@@ -9,20 +9,22 @@ les 3 inputs sont créés dans 3 'div' dont les ids sont 'parentd', 'parentm' et
 Ces 4 'div' doivent exister dans le document HTML pour que ça fonctionne.
 */
 
-export class InputTextDMS {
+export class InputTextDMSHMS {
   element;
   elementD;
   elementM;
   elementS;
+  ratioDMSHMS;
 
-  constructor(id, parent, value) {
+  constructor(parent, value, isDMS = true) {
     //permet aux methodes de connaître "this" !
     //https://stackoverflow.com/questions/4011793/this-is-undefined-in-javascript-class-methods
     this.changeDegre = this.changeDegre.bind(this);
     this.changeDMS = this.changeDMS.bind(this);
 
+    isDMS ? this.ratioDMSHMS = 1 : this.ratioDMSHMS = 15;
+
     this.element = document.createElement('input');
-    this.element.id = id;
     this.element.type = "text";
     this.element.value = value;
   
@@ -30,21 +32,18 @@ export class InputTextDMS {
     document.getElementById(parent).appendChild(this.element);
 
     this.elementD = document.createElement('input');
-    this.elementD.id = id + "d";
-    this.elementD.type = "text";
+     this.elementD.type = "text";
     this.elementD.value = "0";
     this.elementD.oninput = this.changeDMS;
     document.getElementById(parent + "d").appendChild(this.elementD);
 
     this.elementM = document.createElement('input');
-    this.elementM.id = id + "m";
-    this.elementM.type = "text";
+     this.elementM.type = "text";
     this.elementM.value = "0";
     this.elementM.oninput = this.changeDMS;
     document.getElementById(parent + "m").appendChild(this.elementM);
 
     this.elementS = document.createElement('input');
-    this.elementS.id = id + "s";
     this.elementS.type = "text";
     this.elementS.value = "0";
     this.elementS.oninput = this.changeDMS;
@@ -60,11 +59,13 @@ export class InputTextDMS {
   }
 
   changeDegre() {
-    var temp, temptrunc;
-    temptrunc = Math.trunc(parseFloat(this.getValue()));
+    var temp = parseFloat(this.getValue()) / this.ratioDMSHMS,
+        temptrunc;
+
+    temptrunc = Math.trunc(temp);
     this.elementD.value = temptrunc;
   
-    temp = (parseFloat(this.getValue()) - temptrunc) * 60;
+    temp = (temp - temptrunc) * 60;
     temptrunc = Math.trunc(temp);
     this.elementM.value = temptrunc;
   
@@ -73,7 +74,7 @@ export class InputTextDMS {
   }
 
   changeDMS() {
-    this.setValue(parseFloat(this.elementD.value) + parseFloat(this.elementM.value) / 60 + parseFloat(this.elementS.value) / 3600);
+    this.setValue(this.ratioDMSHMS * (parseFloat(this.elementD.value) + parseFloat(this.elementM.value) / 60 + parseFloat(this.elementS.value) / 3600));
   }
   
 }
